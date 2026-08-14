2e0d37185cbc75a4f940f87eca1f12dbc7335e06
sarvesh <sarvesh2005@gmail.com>
2026-08-14 09:31:54 +0530

    changed game and blocks code to class/OOPs based

--- game.js
+++ game.js
@@ -120,29 +120,82 @@ function DrawGrid(){
 }
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+/*
 let block_X = 2;
 let block_Y = 0;
 let block_type = L;
 let block_Speed = 1000;
 let block_Direction = 0;
-
+*/
 class tetriminos{
     //TODO
     //change the way the blocks Path2D obj representation to simplify adding to the stack, 
     //move existing block logic into this class
+    block_type;
+    context;
+    position = [];
+    block_Y = 1;
+    block_X = 2;
+    block_Direction = 0;
+    block_Speed = 1000;
     
-    //constructor()
+    constructor(type){
+        console.log("block created");
+        this.block_type = type
+    }
+
+    draw_shape(){
+        ctx.save();
+        //ctx.translate(-shape.center[0]*unit.width,-shape.center[1]*unit.height);
+        ctx.translate((this.block_X+this.block_type.centers[this.block_Direction][0])*unit.width+0.5,(this.block_Y+this.block_type.centers[this.block_Direction][1])*unit.height+0.5);
+        ctx.rotate((Math.PI/2)*this.block_Direction);
+        ctx.fillStyle = this.block_type.color;
+        ctx.fill(this.block_type.shape);
+        ctx.restore();
+
+        ctx.save();
+        ctx.strokeStyle = "#ff0000";
+        ctx.strokeRect(this.block_X*unit.width+0.5,this.block_Y*unit.width+0.5,20,20);
+        ctx.restore();
+    }
+
+    async drop(){
+
+        while(this.block_Y<8){
+            await delay(this.block_Speed);
+            this.block_Y += 1;
+        }
+    }
+
     //drop/movement()
     //colision()
 }
 class game{
     
+    player_block = new tetriminos(L);
+    PLAY = false;
+
+    constructor(){
+        console.log("game created");
+        console.log(this.player_block);
+    }
+
+    async main(){
+        console.log("game start in class");
+        this.player_block.drop();
+        while (this.PLAY){
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            DrawGrid();
+            this.player_block.draw_shape();
+            await delay(100);
+        }
+    }
     //score calculation
     //block queue(upto 3 blocks)
     //stack (how to save the blocks)
     //clear stack logic
 }
-
+/*
 function draw_shape(x,y,shape){
     ctx.save();
     //ctx.translate(-shape.center[0]*unit.width,-shape.center[1]*unit.height);
@@ -160,7 +213,7 @@ function draw_shape(x,y,shape){
 
 
 async function drop(){
-    
+
     while(block_Y<8){
         await delay(block_Speed);
         block_Y += 1;
@@ -168,11 +221,17 @@ async function drop(){
 }
 
 let PLAY = false;
+*/
+
+
+Gcode = new game();
 
 function toggleGame() {
-    PLAY = !PLAY;
-    if (PLAY) {
-        Game(); // Kickstart the loop only when turning ON
+    Gcode.PLAY = !Gcode.PLAY;
+    console.log(Gcode.PLAY);
+    if (Gcode.PLAY) {
+        console.log("game.start");
+        Gcode.main(); // Kickstart the loop only when turning ON
     }
 }
 
@@ -189,26 +248,26 @@ async function Game(){
 
 function HandleKeys(event){
     if (event.key === "ArrowDown"){
-        block_Speed = 500;
+        Gcode.player_block.block_Speed = 500;
     }else if (event.key === "ArrowUp"){
-        block_Direction = (block_Direction+1)%4;
+        Gcode.player_block.block_Direction = (Gcode.player_block.block_Direction+1)%4;
     }else if (event.key === "r"){
-        block_Speed = 1000;
+        Gcode.player_block.block_Speed = 1000;
         drop();
     }else if (event.key === "ArrowRight"){
-        if (block_X <5){
-            block_X +=1;
+        if (Gcode.player_block.block_X <5){
+            Gcode.player_block.block_X +=1;
         }
     }else if (event.key === "ArrowLeft"){
-        if (block_X > 1){
-            block_X -=1;
+        if (Gcode.player_block.block_X > 1){
+            Gcode.player_block.block_X -=1;
         }
     }else if (event.key === ""){
-        if (block_X > 2){
-            block_X -=1;
+        if (Gcode.player_block.block_X > 2){
+            Gcode.player_block.block_X -=1;
         }
     }else{
-        block_Speed = 1000;
+        Gcode.player_block.block_Speed = 1000;
     }
 }
 document.addEventListener("keydown",HandleKeys)
